c3be14a016d652259ed9065f22771912f0fefe61
you <you@example.com>
2024-10-17 13:06:03 -0700

    fixing new module by renaming its Main.kt into Mod6Main.kt

--- tut2demo32mod6/build.gradle.kts
+++ tut2demo32mod6/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "Mod6MainKt"
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
