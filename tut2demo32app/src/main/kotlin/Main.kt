8d11cd4564f35da0365c3d4e917dff73f5732b63
you <you@example.com>
2024-10-26 10:55:26 -0700

    adding a text line to the top and bottom of scrollable column

--- tut2demo32app/src/main/kotlin/Main.kt
+++ tut2demo32app/src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -9,6 +13,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.example.tut2demo32mod5.Mod5AppFace
@@ -20,7 +27,16 @@ fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Column {
+        Column(
+            modifier = Modifier.padding(10.dp)
+                .background(Color.LightGray)
+                .verticalScroll(rememberScrollState())
+        ) {
+            // the Column modifier can add a ".size(400.dp)" line
+
+            Row {
+                Text("---- ----")
+            }
             Row {
                 Button(onClick = {
                     text = "Hello, Desktop!"
@@ -33,6 +49,9 @@ fun App() {
             }
             Row {
                 Mod6AppFace()
+            }
+            Row {
+                Text("---- ----")
             }
         }
     }
